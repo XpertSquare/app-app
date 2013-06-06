b335f85d19a97e29db815bf1657991f92495a4db
Marius Serban - Office <marius.serban@gmail.com>
2013-06-06 16:05:43 -0700

    Add remember me cookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
   has_many :memberships
   has_many :accounts, :through => :memberships, :dependent => :destroy
   
+   before_create { generate_token(:auth_token) }
+  
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+  
 end

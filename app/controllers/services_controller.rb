f1ad031fa56175b346e45d9459be62e6cc3221c5
Marius Serban <marius.serban@gmail.com>
2013-07-31 21:58:15 -0700

    Add new/create actions to services contoller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -11,4 +11,27 @@ class ServicesController < ApplicationController
       end
     
   end
+  
+   def new
+    @service = Service.new    
+  end
+  
+    def create
+    @service = Service.new(params[:service])
+    @service.account_id = params[:account_id]
+
+    respond_to do |format|
+      if @service.save
+        
+        
+        format.html { redirect_to services_path, :account_id => params[:account_id], notice: 'The service ' + @service.name + ' was successfully created.' }
+        
+      else
+        format.html { render action: "new" }        
+      end
+    end    
+  end
+  
+  
+  
 end
